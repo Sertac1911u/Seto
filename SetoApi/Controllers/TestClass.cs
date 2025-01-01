0c9d1e9dd43e3ced6233b5c4402281cf350835bd
Sertac1911u <114613689+Sertac1911u@users.noreply.github.com>
2025-01-01 22:40:58 +0300

    sadsa

--- SetoApi/Controllers/TestClass.cs
+++ SetoApi/Controllers/TestClass.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SetoClass
+namespace SetoApi.Controllers
 {
-    internal class TestClass
+    public class TestClass
     {
         public int MyProperty { get; set; }
         public int MyProperty2 { get; set; }

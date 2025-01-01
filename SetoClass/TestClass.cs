8e741c92643913834aa8c3bf9ae11091ba6abff0
Sertac1911u <114613689+Sertac1911u@users.noreply.github.com>
2025-01-01 22:38:24 +0300

    test1

---
+++ SetoClass/TestClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetoClass
+{
+    internal class TestClass
+    {
+        public int MyProperty { get; set; }
+    }
+}
